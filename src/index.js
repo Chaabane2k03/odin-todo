2634177ec14d1bb0760bfa9609b7b27befa45900
Chaabane2k03 <chaabaneboussadia@gmail.com>
2024-06-07 22:26:38 +0100

    Start HTML design for the main page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("okay");
+import { format, compareAsc } from "date-fns";
+
+format(new Date(2014, 1, 11), "MM/dd/yyyy");
+//=> '02/11/2014'
+
+const dates = [
+  new Date(1995, 6, 2),
+  new Date(1987, 1, 11),
+  new Date(1989, 6, 10),
+];
+dates.sort(compareAsc);
+
+
+//Testing module 'date-fns' that is working on webpack 

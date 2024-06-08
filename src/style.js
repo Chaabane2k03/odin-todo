e4de13350ae49c99f2b8942653af3551bb34c799
Chaabane2k03 <chaabaneboussadia@gmail.com>
2024-06-08 14:39:42 +0100

    Finishing the Logic of the app Reamaining : linking the logic with the user interface to finish the project

---
+++ src/style.js
@@ -0,0 +1,7 @@
+//Footer :
+document.getElementById("liveToastBtn").onclick=function(){
+    var myAlert = document.getElementById('liveToast');
+    var bsAlert = new bootstrap.Toast(myAlert);
+
+    bsAlert.show();
+}

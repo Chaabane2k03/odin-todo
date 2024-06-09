dc5ec62ab5a417918dcdfb147d452f7cfe179e2a
Chaabane2k03 <chaabaneboussadia@gmail.com>
2024-06-09 15:49:47 +0100

    implementing some methods in JavaScript

--- src/UI.js
+++ src/UI.js
@@ -45,16 +45,61 @@ export default class UI{
 
     static loadProjectContent(projectName){
         const projectPreview = document.getElementById('project-preview')
-        projectPreview.innerHTML = '<h1 id="project-name" class="project-name">${projectName}</h1> '
-        projectPreview.innerHTML += '<div class="tasks-list" id="tasks-list"></div>'
-
+        projectPreview.innerHTML = `
+            <h1 id="project-name" class="project-name">
+                <span> ${projectName} </span>
+            </h1>
+            <div class="tasks-list" id="tasks-list"></div> 
+        `
         if(projectName !== 'Today' && projectName !== 'This week'){
-            projectPreview.innerHTML += '<div class="add-task-popup"><button class="button-add-task btn btn-outline-secondary" id="button-add-task"><i class="fas fa-plus"></i>Add Task</button></div>'
+            projectPreview.innerHTML += `
+            <div class="add-task-popup">
+                <button class="button-add-task btn btn-outline-secondary" id="button-add-task">
+                    <i class="fas fa-plus"></i>
+                    Add Task
+                </button>
+            </div>
+            <div class="input-group add-task-input-popup">
+                <input type="text" class="form-control" placeholder="Hang out with friends" aria-label="Recipient's username with two button addons">
+                <button class="btn btn-outline-success" type="button">Add</button>
+                <button class="btn btn-outline-danger" type="button">Cancel</button>
+            </div>
+            `;
+
+
             
         }
+        UI.loadTasks(projectName);
     }
 
-    //Creating content :
+
+
+    //CREATING CONTENT :
+
+    //create the project :
+    static createProject(name){
+        const userProjects = document.getElementById('projects-list')
+        userProjects.innerHTML += `
+        <button type="button" class="btn btn-danger btn-lg odin-btn odin-added-btn">       
+            <div class="left-project-panel">
+                <i class="fas fa-check"></i>
+                <span>${name}</span>
+            </div>
+            <div class="right-project-panel">
+                <i class="fas fa-times"></i>
+            </div>
+        </button>
+        `
+        UI.initProjectButtons()
+    }
+
+    static createTask(name , dueDate){
+        const tasksList = document.getElementById('tasks-list')
+        //TODO : still the design of a task and the rest of the function 
+        tasksList.innerHTML += `
+
+        `
+    }    
     //Clearing the content
     //Handling popups and Inputs :
     //Project event listeners :
